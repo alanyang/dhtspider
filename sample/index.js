46013de2fb21a5942243ecbc8475aba068269609
AlanYang <alanyang@AlanYangs-MacBook-Pro.local>
2016-04-28 08:19:08 +0800

    fixed bug

--- sample/index.js
+++ sample/index.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const spider = new (require('../spider'))
+const spider = new (require('../lib/spider'))
 
 spider.on('ensureHash', (hash, addr)=> console.log(`magnet:?xt=urn:btih:${hash}`))
 
-spider.on('unensureHash', (hash)=> console.log(hash))
+// spider.on('unensureHash', (hash)=> console.log(hash))
 
-spider.listen(2412)
+// spider.on('nodes', (nodes)=>console.log('foundNodes'))
+
+spider.listen(6339)
